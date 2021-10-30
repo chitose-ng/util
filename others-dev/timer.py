4bc41719db1cf89347dd76e61bdcc2f9a1e99f91
chitose-ng <k.nagano1020@gmail.com>
2021-10-30 11:33:12 +0900

    New: Timerを追加

---
+++ others-dev/timer.py
@@ -0,0 +1,34 @@
+# https://www.guruguru.science/competitions/16/discussions/95b7f8ec-a741-444f-933a-94c33b9e66be/
+# 厳密には https://github.com/nyk510/vivid/blob/master/vivid/utils.py
+
+from time import time
+
+class Timer:
+    def __init__(self, logger=None, format_str='{:.3f}[s]', prefix=None, suffix=None, sep=' ', verbose=0):
+
+        if prefix: format_str = str(prefix) + sep + format_str
+        if suffix: format_str = format_str + sep + str(suffix)
+        self.format_str = format_str
+        self.logger = logger
+        self.start = None
+        self.end = None
+        self.verbose = verbose
+
+    @property
+    def duration(self):
+        if self.end is None:
+            return 0
+        return self.end - self.start
+
+    def __enter__(self):
+        self.start = time()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end = time()
+        if self.verbose is None:
+            return
+        out_str = self.format_str.format(self.duration)
+        if self.logger:
+            self.logger.info(out_str)
+        else:
+            print(out_str)
